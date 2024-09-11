5c1bfec87f7a0165e4f0b7c366a8e7602d082b58
David Essien <davidessienshare@gmail.com>
2024-09-11 12:06:06 +0100

    update authentication schema

--- src/authentication/schemas.py
+++ src/authentication/schemas.py
@@ -1,27 +1,62 @@
 from datetime import datetime
-from pydantic import EmailStr, BaseModel, Field
 from typing import Optional, List
+
+from pydantic import EmailStr, BaseModel, Field
 
 # Schema for creating a new user
 
 
 class UserCreate(BaseModel):
-    username: str = Field(max_length=20)
-    email: EmailStr = Field(max_length=40)
-    phone: Optional[str] = None
-    # Corrected typo from mine_length to min_length
-    password: str = Field(min_length=8)
+    """
+    Schema for creating a new user.
 
+    Attributes:
+        username (str): The desired username for the new user. Must be between 1 and 20 characters.
+        email (EmailStr): The email address of the new user. valid email up to 40 characters.
+        phone (Optional[str]): The phone number of the new user. Optional field.
+        password (str): The password for the new user. Must be at least 8 characters long.
+    """
+    username: str = Field(..., max_length=20)
+    email: EmailStr = Field(..., max_length=40)
+    phone: Optional[str] = None
+    password: str = Field(..., min_length=8)
 
 # Schema for user login
-class UserLogin(BaseModel):
-    email: EmailStr = Field(max_length=40)
-    # Corrected typo from mine_length to min_length
-    password: str = Field(min_length=8)
 
+
+class UserLogin(BaseModel):
+    """
+    Schema for user login.
+
+    Attributes:
+        email (EmailStr): email address
+        password (str): password, must be at least 8 characters.
+    """
+    email: EmailStr = Field(..., max_length=40)
+    password: str = Field(..., min_length=8)
 
 # Schema for reading user data (response)
+
+
 class UserRead(BaseModel):
+    """
+    Schema for reading user data.
+
+    Attributes:
+        id (int): The unique identifier for the user.
+        username (str): The username of the user.
+        email (str): The email address of the user.
+        email_verified (bool): Whether the user's email has been verified.
+        phone (Optional[str]): The phone number of the user, if available.
+        phone_verified (bool): Whether the user's phone number has been verified.
+        is_active (bool): Whether the user's account is currently active.
+        roles (Optional[List]): A list of roles associated with the user.
+        created_at (datetime): Timestamp of when the user was created.
+        updated_at (datetime): Timestamp of when the user was last updated.
+
+    Config:
+        from_attributes (bool): Allows compatibility for reading models from SQLModel/ORM models.
+    """
     id: int
     username: str
     email: str
@@ -29,21 +64,39 @@ class UserRead(BaseModel):
     phone: Optional[str] = None
     phone_verified: bool
     is_active: bool
+    roles: Optional[List] = []
     created_at: datetime
     updated_at: datetime
 
     class Config:
-        from_attributes = True  # Allows SQLModel/ORM compatibility for reading models
+        """
+        map to orm models
+        """
+        from_attributes = True
 
 # Schema for updating a user's data
 
 
 class UserUpdate(BaseModel):
-    username: Optional[str] = Field(max_length=20)
-    email: Optional[EmailStr] = Field(max_length=40)
+    """
+    Schema for updating a user's data.
+
+    Attributes:
+        username (Optional[str]): optional username, must be between 1 and 20 characters if provided
+        email (Optional[EmailStr]): optional email address
+        phone (Optional[str]): The new phone number for the user. Optional field.
+        password (Optional[str]): optional password
+
+    Config:
+        from_attributes (bool): Allows compatibility for reading models from SQLModel/ORM models.
+    """
+    username: Optional[str] = Field(None, max_length=20)
+    email: Optional[EmailStr] = Field(None, max_length=40)
     phone: Optional[str] = None
-    # Only required if updating the password
-    password: Optional[str] = Field(min_length=8)
+    password: Optional[str] = Field(None, min_length=8)
 
     class Config:
+        """
+        map to orm models
+        """
         from_attributes = True
