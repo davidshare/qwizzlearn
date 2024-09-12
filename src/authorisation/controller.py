fffbfc67e13df7e261ab160362d53267fb86ca6c
David Essien <davidessienshare@gmail.com>
2024-09-12 05:42:19 +0100

    delete role

--- src/authorisation/controller.py
+++ src/authorisation/controller.py
@@ -117,17 +117,23 @@ class AuthorisationController:
             ) from exc
 
     @staticmethod
+    async def delete_role(role_id: int, session: AsyncSession):
+        if not role_id:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Role ID missing: please provide the id of the role."
+            )
+        try:
+            return await authorisation_service.delete_role(role_id, session)
+        except RoleNotFoundException as e:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Role not found") from e
+
+    @staticmethod
     async def assign_permission_to_role(role_id: int, permission_id: int, session: AsyncSession):
         role = await authorisation_service.assign_permission_to_role(role_id, permission_id, session)
         if not role:
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND, detail="Role or Permission not found")
         return role
-
-    @staticmethod
-    async def delete_role(role_id: int, session: AsyncSession):
-        role = await authorisation_service.get_role_by_id(role_id, session)
-        if not role:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND, detail="Role not found")
-        return await authorisation_service.delete_role(role_id, session)
