c57167e404d130670388af489a23c5f858983c60
David Essien <davidessienshare@gmail.com>
2024-09-13 21:36:45 +0100

    chore(groups): setup groups
    - add groups models
    - add groups schemas
    - add groups migrations

---
+++ src/groups/schemas.py
@@ -0,0 +1,43 @@
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional, List
+
+
+class GroupBase(BaseModel):
+    name: str
+    description: Optional[str]
+
+
+class GroupCreate(GroupBase):
+    created_by: int
+
+
+class GroupRead(GroupBase):
+    id: Optional[int]  # Marking id as optional for read
+    created_by: int
+    created_at: datetime
+    updated_at: datetime
+    users: Optional[List["GroupUserRead"]] = []  # Added list of related users
+
+
+class GroupUpdate(GroupBase):
+    pass
+
+
+class GroupUserBase(BaseModel):
+    group_id: int
+    user_id: int
+    role: str
+
+
+class GroupUserCreate(GroupUserBase):
+    pass
+
+
+class GroupUserRead(GroupUserBase):
+    id: Optional[int]  # Marking id as optional for read
+    joined_at: datetime
+
+
+class GroupUserUpdate(GroupUserBase):
+    pass
