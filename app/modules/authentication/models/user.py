7e3e66386692bfed33f77d7baf70a7cc2b615424
David Essien <davidessienshare@gmail.com>
2025-01-26 14:18:13 +0100

    rename user model

--- app/modules/authentication/models/user.py
+++ app/modules/authentication/models/user.py
@@ -6,14 +6,14 @@ from app.core.mixins import TimestampMixin
 
 
 class User(SQLModel, TimestampMixin, table=True):
-    id: Optional[str] = Field(default=None, primary_key=True)
+    id: Optional[int] = Field(default=None, primary_key=True)
     username: str = Field(unique=True, index=True)
     email: str = Field(unique=True, index=True)
     password_hash: str
     phone_number: Optional[str] = Field(unique=True, default=None)
     email_verified: bool = Field(default=False)
     phone_verified: bool = Field(default=False)
-    is_active: bool = Field(default=True)
+    is_active: bool = Field(default=False)
     is_locked: bool = Field(default=False)
     mfa_secret_key: Optional[str] = Field(default=None)
     mfa_enabled: bool = Field(default=False)
